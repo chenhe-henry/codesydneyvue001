13953e73ef7b7b08a5144300b0a1e5e2dae24069
chenhe-henry <chenhe.elec@gmail.com>
2020-07-13 12:10:26 +1000

    add services folder with ProjectService.js, to fetch api calls

--- src/pages/Newsletter.vue
+++ src/pages/Newsletter.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="newsletter">
     <BaseBanner>🏗 Newsletter Page is Coming Soon</BaseBanner>
+    <div v-for="project in project" :key="project.id">
+      {{ project.id }}: {{ project.title }}
+    </div>
   </div>
 </template>
 <script>
+import ProjectService from "../services/ProjectService";
 export default {
   data() {
-    return {};
+    return {
+      project: {},
+    };
   },
   components: {},
+  created() {
+    ProjectService.getProjects()
+      .then((res) => {
+        this.project = res.data;
+        this.project.length = 9;
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
 };
 </script>
 
